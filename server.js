63db3e21ae0145c741945f3007915fd3d63851b7
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-09-15 17:14:16 +0530

    setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const app = require('express')();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to DB'))
+    .catch(err => {
+        console.log(err, "mongo_error");
+    });
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
