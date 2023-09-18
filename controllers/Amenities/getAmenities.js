904427dfca4c5bf823055f6eee84646ee53ca5ba
preet-patil-4055 <preet.patil@retvensservices.com>
2023-09-18 17:25:14 +0530

    add models

---
+++ controllers/Amenities/getAmenities.js
@@ -0,0 +1,1 @@
+const amenities = require('../../models/Amenities/amenities')
---
+++ models/Rooms/bedTypes.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const randomstring = require("randomstring");
+
+
+const bedTypeSchema = new mongoose.Schema({
+
+  bedTypeId: { type: String, default: randomstring.generate(10), unique: true },
+
+  bedTypeName: { type: String, default: "", required: false },
+
+  bedTypeIcon: { type: String, default: "", required: false },
+
+  bedTypeSize:{type: String, default: ''},
+
+  bedTypeSortCode : { type: String, default: "", required: false, unique: true },
+
+  createdBy : { type: String, default: "", required: false },
+
+  modifiedBy : [{ type: String, default: "", required: false }],
+
+  modifiedDate : [{ type: String, default: "", required: false }]
+
+
+});
+
+const bedTypes = mongoose.model("bedtypes", bedTypeSchema)
+module.exports = bedTypes
