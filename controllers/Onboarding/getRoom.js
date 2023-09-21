114322a5450bfb6c46ea030bf0d8138a26bd963b
preet-patil-4055 <preet.patil@retvensservices.com>
2023-09-22 01:54:52 +0530

    update api response

--- controllers/Onboarding/getRoom.js
+++ controllers/Onboarding/getRoom.js
@@ -1,15 +1,44 @@
-const Property =require('../../models/Onboarding/roomTypeDetails')
+const Property = require('../../models/Onboarding/roomTypeDetails')
 
-module.exports= async (req,res)=>{
-   try{
-    const roomTypeId =req.params.roomTypeId
-const room =await Property.findOne({roomTypeId})
-if(!room){
-    return res.status(404).json({error:'Room not found'})
-}
-return res.status(200).json(room);
-   }
-   catch(error){
-    return res.status(500).json({error:error.message})
-   }
+module.exports = async (req, res) => {
+    try {
+        const roomTypeId = req.params.roomTypeId;
+        const room = await Property.findOne({ roomTypeId });
+        if (!room) {
+            return res.status(404).json({ error: 'Room not found' });
+        }
+        
+        // Extract the first item from each array
+        const {
+            description,
+            numberOfRooms,
+            bedType,
+            roomSize,
+            smoking,
+            roomName,
+            roomType,
+            generalAmenities,
+            propertyId,
+            userId
+        } = room;
+
+        // Create a new object with extracted data
+        const extractedData = {
+            userId,
+            propertyId,
+            roomTypeId,
+            description: description[0] || {},
+            numberOfRooms: numberOfRooms[0] || {},
+            bedType: bedType[0] || {},
+            roomSize: roomSize[0] || {},
+            smoking: smoking[0] || {},
+            roomName: roomName[0] || {},
+            roomType: roomType[0] || {},
+            generalAmenities
+        };
+
+        return res.status(200).json(extractedData);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 }
