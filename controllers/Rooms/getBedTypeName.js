506b049817f59ff20c8a86536e4fe0801d29d124
preet-patil-4055 <preet.patil@retvensservices.com>
2023-09-24 02:02:26 +0530

    update bedtype get api

--- controllers/Rooms/getBedTypeName.js
+++ controllers/Rooms/getBedTypeName.js
@@ -1,9 +1,9 @@
 //models path
 const bedType = require('../../models/Rooms/bedTypes')
 
- module.exports=async (req, res) => {
+module.exports = async (req, res) => {
   try {
-    const bedName = await bedType.find({}, { bedTypeName: 1, _id: 0 }).lean().exec(); 
+    const bedName = await bedType.find({}, { bedTypeName: 1, bedTypeId: 1, _id: 0 }).lean().exec();
     // Fetch all country names
     res.status(200).json(bedName);
   } catch (err) {
