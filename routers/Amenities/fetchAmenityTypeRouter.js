6ac6fab9ec1fbaa4205a3f8985630c9576edaf72
preet-patil-4055 <preet.patil@retvensservices.com>
2023-09-25 00:58:32 +0530

    update amenities and inclusions api

--- routers/Amenities/fetchAmenityTypeRouter.js
+++ routers/Amenities/fetchAmenityTypeRouter.js
@@ -2,5 +2,5 @@ const { Router } = require('express');
 const fetchAmenitiesType = require('../../controllers/Amenities/fetchAmenityType');
 const app = Router();
 
-app.get('/fetchAmenityType/:category',fetchAmenitiesType);
+app.get('/fetchAmenityType/:type/:propertyId',fetchAmenitiesType);
 module.exports = app;
