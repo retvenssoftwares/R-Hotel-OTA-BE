09d8cf08bee116ca42cc7a86e1d34bcccf62a96f
Pranay9752 <pranayambre01@gmail.com>
2023-09-24 23:26:22 +0530

    Update

--- controllers/Rooms/fetchRoomTypeByPropertyId.js
+++ controllers/Rooms/fetchRoomTypeByPropertyId.js
@@ -10,10 +10,13 @@ module.exports = async (req, res) => {
         if (!rooms || rooms.length === 0) {
             return res.status(404).json({ error: 'No rooms found for the given propertyId' });
         }
-        
+
         // Extract only the "roomType" field from each record
         const extractedData = {
-            roomType: rooms.map(room => room.roomType[0] ? room.roomType[0].roomType : ''),
+            roomType: rooms.map(room => ({
+                    roomName: room.roomName[0] ? room.roomName[0].roomName : '',
+                    roomTypeId: room.roomTypeId
+                })),
         };
 
         return res.status(200).json(extractedData);
