5c8130eaa84e51a4720b54037cc14ed0d41d0b69
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-09-25 00:50:11 +0530

    update

--- controllers/Rooms/fetchRateTypeByRateId.js
+++ controllers/Rooms/fetchRateTypeByRateId.js
@@ -3,24 +3,24 @@ const RateType = require('../../models/Rooms/rateType')
 module.exports = async (req, res) => {
     try {
         const rateTypeId = req.params.rateTypeId;
-        const rateTypes = await RateType.find({ rateTypeId: rateTypeId });
+        const rateTypes = await RateType.findOne({ rateTypeId: rateTypeId });
 
         if (!rateTypes || rateTypes.length === 0) {
             return res.status(404).json({ error: 'No rate type found for the given id' });
         }
 
         // Extract data from all rate plans
-        const extractedData = rateTypes.map(ratePlan => ({
-            date: ratePlan.date,
-            propertyId: ratePlan.propertyId,
-            rateTypeId: ratePlan.rateTypeId,
-            roomTypeId: ratePlan.roomTypeId,
-            name: ratePlan.name[0] || {},
-            basePrice: ratePlan.basePrice[0] || {},
-            roomType: ratePlan.roomType[0] || {},
-            taxIncluded: ratePlan.taxIncluded[0] || {},
-            refundable: ratePlan.refundable[0] || {},
-        }));
+        const extractedData ={
+            date: rateTypes.date,
+            propertyId: rateTypes.propertyId,
+            rateTypeId: rateTypes.rateTypeId,
+            roomTypeId: rateTypes.roomTypeId,
+            name: rateTypes.name[0] || {},
+            basePrice: rateTypes.basePrice[0] || {},
+            roomType: rateTypes.roomType[0] || {},
+            taxIncluded: rateTypes.taxIncluded[0] || {},
+            refundable: rateTypes.refundable[0] || {},
+        };
 
         return res.status(200).json(extractedData);
     } catch (error) {
