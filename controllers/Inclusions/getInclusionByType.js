ed6310343590c7edd771a6068cc2d460251d0534
preet-patil-4055 <preet.patil@retvensservices.com>
2023-09-25 12:39:26 +0530

    update fetch inclusion api

--- controllers/Inclusions/getInclusionByType.js
+++ controllers/Inclusions/getInclusionByType.js
@@ -1,17 +1,3 @@
-// const inclusionModel = require('../../models/Inclusions/inclusions');
-
-// module.exports = async (req, res) => {
-//     try {
-//         const findInclusion = await inclusionModel.find({ inclusionType: req.params.inclusionType }).select('inclusionId inclusionName inclusionType inclusionDescription')
-//         if (findInclusion) {
-//             return res.status(200).json(findInclusion)
-//         } else {
-//             return res.status(404).json({ message: "No inclusions found of this type" })
-//         }
-//     } catch (err) {
-//         return res.status(500).json({ message: "Internal Server Error" })
-//     }
-// }
 
 const inclusionModel = require('../../models/Inclusions/inclusions');
 const ratePlanModel = require('../../models/Rooms/ratePlan');
@@ -28,41 +14,61 @@ module.exports = async (req, res) => {
             { inclusionId: 1, inclusionName: 1, inclusionType: 1, inclusionDescription: 1, isSelected: 1 }
         );
 
+        if(!allInclusions){
+            return res.status(404).json({message: "No inclusions found of this type."})
+        }
+
         // Retrieve the amenities array for the specified propertyId
         const ratePlanInclusions = await ratePlanModel.findOne(
             { ratePlanId: ratePlanId },
             { inclusion: 1 }
         );
-
-        if (!ratePlanInclusions) {
-            return res.status(404).json({ error: "rate plan not found" });
-        }
+      
 
         // Create an array to store the results
         const result = [];
+        if (ratePlanId === 'default') {
+            // Loop through all amenities and determine their status based on property amenities
+            for (const inclusionData of allInclusions) {
+                const status = false;
+                result.push({
+                    inclusionName: inclusionData.inclusionName,
+                    inclusionId: inclusionData.inclusionId,
+                    inclusionDescription: inclusionData.inclusionDescription,
+                    inclusionType: inclusionData.inclusionType,
+                    status: status
+                });
+            }
 
-        // Loop through all amenities and determine their status based on property amenities
-        for (const inclusionData of allInclusions) {
-            const isSelected = ratePlanInclusions.inclusion.some(
-                (ratePlanIncusion) =>
-                    ratePlanIncusion.inclusionId === inclusionData.inclusionId &&
-                    ratePlanIncusion.isSelected === 'true'
-            );
+            res.status(200).json(result);
+        } else {
+            // Loop through all amenities and determine their status based on property amenities
+            if (!ratePlanInclusions) {
+                return res.status(404).json({ error: "rate plan not found" });
+            }
+            for (const inclusionData of allInclusions) {
+                const isSelected = ratePlanInclusions.inclusion.some(
+                    (ratePlanIncusion) =>
+                        ratePlanIncusion.inclusionId === inclusionData.inclusionId &&
+                        ratePlanIncusion.isSelected === 'true'
+                );
 
-            // Determine the status based on isSelected
-            const status = isSelected ? true : false;
+                // Determine the status based on isSelected
+                const status = isSelected ? true : false;
 
-            result.push({
-                inclusionName: inclusionData.inclusionName,
-                inclusionId: inclusionData.inclusionId,
-                inclusionDescription: inclusionData.inclusionDescription,
-                inclusionType: inclusionData.inclusionType,
-                status: status
-            });
+                result.push({
+                    inclusionName: inclusionData.inclusionName,
+                    inclusionId: inclusionData.inclusionId,
+                    inclusionDescription: inclusionData.inclusionDescription,
+                    inclusionType: inclusionData.inclusionType,
+                    status: status
+                });
+            }
+            res.status(200).json(result);
         }
 
-        res.json(result);
     } catch (error) {
+        console.log(error)
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
