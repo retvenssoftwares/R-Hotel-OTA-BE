6d9803f58bd9e5fb0b27475f6b21320dd8d20da1
preet-patil-4055 <preet.patil@retvensservices.com>
2023-09-26 23:34:35 +0530

    get property by city

--- controllers/manageInventory/getRateByPropertyId.js
+++ controllers/manageInventory/getRateByPropertyId.js
@@ -1,47 +1,4 @@
-// const rate = require('../../models/manageInventory/manageRates');
-// const rateTypeModel = require('../../models/Rooms/rateType')
-
-// module.exports = async (req, res) => {
-//     try {
-//         const roomTypeId = req.params.roomTypeId;
-//         const startDate = req.query.startDate;
-
-//         if (!startDate) {
-//             return res.status(400).json({ error: 'Start date is required as a query parameter.' });
-//         }
-
-//         const manageRate = await rate.find({
-//             roomTypeId: roomTypeId,
-//             // $or: [
-//             //     { 'manageInventory.modifiedDate': { $gte: startDate } },
-//             //     { 'manageInventory': { $exists: false, $size: 0 } }
-//             // ]
-//         });
-
-//         if (!manageRate || manageRate.length === 0) {
-//             return res.status(404).json({ error: 'No Rate found for the given propertyId and date range.' });
-//         }
-
-//         // Extract ratesAndInventory data within the specified date range
-//         const extractedData = manageRate.map(ratePlan => ({
-//             date: ratePlan.date,
-//             propertyId: ratePlan.propertyId,
-//             rateTypeId: ratePlan.rateTypeId,
-//             roomTypeId: ratePlan.roomTypeId,
-//             ratePrice: ratePlan.ratePrice[0] || {},
-//             manageRate: ratePlan.manageRate
-//                 ? ratePlan.manageRate
-//                       .filter(item => item.modifiedDate >= startDate)
-//                       .slice(0, 7)
-//                 : [] // Empty array if manageInventory is not defined
-//         }));
-
-//         return res.status(200).json(extractedData);
-//     } catch (error) {
-       
-//         return res.status(500).json({ error: error.message });
-//     }
-// };
+;
 
 const rate = require('../../models/manageInventory/manageRates');
 const rateTypeModel = require('../../models/Rooms/rateType');
@@ -63,7 +20,7 @@ module.exports = async (req, res) => {
             return res.status(404).json({ error: 'No Rate found for the given roomTypeId.' });
         }
 
-        // Extract ratesAndInventory data within the specified date range
+        // Extract rates data within the specified date range
         const extractedData = await Promise.all(manageRate.map(async (ratePlan) => {
             const rateType = await rateTypeModel.findOne({ rateTypeId: ratePlan.rateTypeId });
 
