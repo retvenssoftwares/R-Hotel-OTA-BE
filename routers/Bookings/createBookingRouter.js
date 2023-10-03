ea93a505e12661472d6ae5cffc8971b70d723853
preet-patil-4055 <preet.patil@retvensservices.com>
2023-10-03 12:55:50 +0530

    get all properties api

--- routers/Bookings/createBookingRouter.js
+++ routers/Bookings/createBookingRouter.js
@@ -2,12 +2,7 @@ const { Router } = require('express');
 const addBooking = require('../../controllers/Booking/createBooking');
 const app = Router();
 
-//app.post('/createBooking',addBooking);
+ // Define the route for fetching inventory
+app.post('/createBooking', addBooking)
 
-module.exports = (io) => {
-    // Define the route for fetching inventory
-    app.post('/createBooking', addBooking(io))
-
-    return app;
-};
-
+module.exports =  app
