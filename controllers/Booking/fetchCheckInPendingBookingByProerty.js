bc4fc556bd2242ac7b0e068b3c1d0172a87186ee
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-10-04 16:03:16 +0530

    update

--- controllers/Booking/fetchCheckInPendingBookingByProerty.js
+++ controllers/Booking/fetchCheckInPendingBookingByProerty.js
@@ -16,7 +16,7 @@ module.exports = async function fetchBookingsByPropertyId(req, res) {
         checkOutDate: { $lte: endDate },    // Check-out date is less than or equal to endDate
       };
 
-      const bookings = await pendingBooking.find(query,{
+      const booking = await pendingBooking.find(query,{
         checkOutDate: 1,
       checkInDate: 1,
       bookingId:1,
@@ -27,8 +27,32 @@ module.exports = async function fetchBookingsByPropertyId(req, res) {
       totalAmount: 1,
       _id: 0, // Exclude the _id field from the response
       });
-      console.log(bookings);
-      res.status(200).json(bookings);
+     // Manually populate the ratePlanName
+     const populatedBookings = await Promise.all(
+      booking.map(async (booking) => {
+        const ratePlanId = booking.roomDetails[0].ratePlanId; // Assuming a single ratePlanId per booking
+        const ratePlanData = await ratePlan.findOne({ ratePlanId: ratePlanId });
+        const ratePlanName = ratePlanData ? ratePlanData.ratePlanName : '';
+        return {
+          ...booking.toObject(),
+          ratePlanName,
+        };
+      })
+    );
+
+    // Remove ratePlanName array and keep only ratePlanName
+    const cleanedBookings = populatedBookings.map((booking) => {
+      const { ratePlanName, ...rest } = booking;
+      return {
+        ...rest,
+        ratePlanName: ratePlanName[0].ratePlanName, // Assuming a single ratePlanName per booking
+      };
+    });
+
+
+
+    console.log(cleanedBookings);
+    res.status(200).json(cleanedBookings);
     } else {
       // If requestType is not "checkIn", send a response
       res.status(400).json({ message: 'Please specify requestType as "checkIn"' });
