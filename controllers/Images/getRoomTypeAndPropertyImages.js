0b4be101fdd9b646d40edfb0b76c8921b7a4f232
Shubhamretvens <shubham.singh@retvensservices.com>
2023-10-05 16:14:44 +0530

    pull

--- controllers/Images/getRoomTypeAndPropertyImages.js
+++ controllers/Images/getRoomTypeAndPropertyImages.js
@@ -2,7 +2,7 @@ const propertyImagesModel = require('../../models/Images/propertyImages')
 const roomTypeImagesModel = require('../../models/Images/roomTypeImages');
 
 module.exports.getRoomTypeImages = async (req, res) => {
-    const getRoomTypeImagesData = await roomTypeImagesModel.findOne({ roomTypeId: req.params.roomTypeId })
+    const getRoomTypeImagesData = await roomTypeImagesModel.findOne({ roomTypeId: req.params.roomTypeId , displayStatus:"1"})
 
     if (!getRoomTypeImagesData) {
         return res.status(200).json({ message: "ID not found" })
@@ -17,7 +17,7 @@ module.exports.getRoomTypeImages = async (req, res) => {
 }
 
 module.exports.propertyImages = async (req, res) => {
-    const getPropertyImages = await propertyImagesModel.findOne({ propertyId: req.params.propertyId })
+    const getPropertyImages = await propertyImagesModel.findOne({ propertyId: req.params.propertyId ,displayStatus:"1"})
 
     if (!getPropertyImages) {
         return res.status(200).json({ message: "ID not found" })
