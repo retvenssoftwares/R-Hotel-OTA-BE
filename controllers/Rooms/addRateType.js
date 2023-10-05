3f33386eda869529acfbf6c32e7cd201d071303b
preet-patil-4055 <preet.patil@retvensservices.com>
2023-10-06 01:55:52 +0530

    update add rate type api

--- controllers/Rooms/addRateType.js
+++ controllers/Rooms/addRateType.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+
 const Property = require("../../models/Onboarding/propertys");
 const rateModel = require("../../models/manageInventory/manageRates");
 const RoomType = require("../../models/Rooms/roomTypeDetails");
@@ -7,86 +6,92 @@ const rateType = require("../../models/Rooms/rateType");
 const admin = require("../../models/Onboarding/registrations");
 const randomstring = require("randomstring");
 
-
 // Create a POST route for user registration
 module.exports = async (req, res) => {
     try {
         // Get user data from the request body
-        const { userId, propertyId, roomTypeId, name, inclusion, basePrice, roomType, taxIncluded, refundable, SessionId, date } = req.body;
+        const { userId, propertyId, roomTypeIds, name, inclusion, basePrice, roomType, taxIncluded, refundable, SessionId, date } = req.body;
 
-        const userProfile = await admin.findOne({ userId: userId })
-        const room = await RoomType.findOne({ roomTypeId: roomTypeId });
-        if (!room) {
-            return res.status(404).json({ message: "RoomType not found" });
-        }
-        const { sessionId } = userProfile
+        const userProfile = await admin.findOne({ userId: userId });
+        const { sessionId } = userProfile;
 
         if (sessionId !== SessionId) {
-            return res.status(404).json({ message: "session id not match" })
+            return res.status(404).json({ message: "Session ID does not match" });
         }
-        let rateTypeId = randomstring.generate(8)
-        // Create a new user using the Mongoose model
-        const newplan = new rateType({
-            rateTypeId: rateTypeId,
-            propertyId,
-            roomTypeId,
 
-            name: [{
-                name: name,
-                modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
-            }],
+        // Define an array to store the promises for each roomType
+        const promises = roomTypeIds.map(async (roomTypeId) => {
+            const room = await RoomType.findOne({ roomTypeId });
 
-            inclusion: [{
-                inclusion: inclusion,
-                modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
-            }],
+            if (!room) {
+                return { success: false, message: "RoomType not found" };
+            }
 
-            basePrice: [{
-                basePrice: basePrice,
-                modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
-            }],
+            let rateTypeId = randomstring.generate(8);
 
-            roomType: [{
-                roomType: roomType,
-                modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
-            }],
+            const newPlan = new rateType({
+                rateTypeId: rateTypeId,
+                propertyId,
+                roomTypeId,
+                name: [{
+                    name: name,
+                    modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                }],
+                inclusion: [{
+                    inclusion: inclusion,
+                    modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                }],
+                basePrice: [{
+                    basePrice: basePrice,
+                    modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                }],
+                roomType: [{
+                    roomType: roomType,
+                    modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                }],
+                taxIncluded: [{
+                    taxIncluded: taxIncluded,
+                    modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                }],
+                refundable: [{
+                    refundable: refundable,
+                    modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                }],
+                date: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }),
+            });
 
-            taxIncluded: [{
-                taxIncluded: taxIncluded,
-                modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
-            }],
+            // Save the new user to the database
+            await newPlan.save();
 
-            refundable: [{
-                refundable: refundable,
-                modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
-            }],
+            // Use Mongoose to update the record and prepend the new object to the array
+            const createRate = new rateModel({
+                propertyId: propertyId,
+                roomTypeId: roomTypeId,
+                rateTypeId: rateTypeId,
+                ratePrice: [{
+                    basePrice: basePrice,
+                    modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                }]
+            });
 
-            date: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }),
+            await createRate.save();
+            
+            return { success: true, message: 'Rate type added successfully' };
         });
 
-        // Save the new user to the database
-        const savedProperty = await newplan.save();
+        // Wait for all promises to resolve
+        const results = await Promise.all(promises);
 
-        // Define the object you want to prepend to the array
-        const rateObject = { basePrice: basePrice, modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }) };
-        // console.log(rateObject)
-        // Use Mongoose to update the record and prepend the new object to the array
-        const createRate = new rateModel({
-            propertyId: propertyId,
-            roomTypeId: roomTypeId,
-            rateTypeId: rateTypeId,
-            ratePrice: [{
-                basePrice: basePrice,
-                modifiedDate: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
-            }]
-        })
-        await createRate.save();
+        // Check if any of the promises failed
+        const failedResult = results.find(result => !result.success);
 
-        res.status(201).json({ message: 'rate type added  successfully' });
+        if (failedResult) {
+            return res.status(404).json({ message: failedResult.message });
+        }
+
+        res.status(201).json({ message: 'All rate types added successfully' });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
-
-
